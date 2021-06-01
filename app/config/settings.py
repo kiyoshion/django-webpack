fac7349b773804f0f06375a5f91bf2c6798e5fa7
kiyoshion <kiyoshion@gmail.com>
2021-06-01 19:15:40 +0900

    Update ALLOWED_HOSTS

--- app/config/settings.py
+++ app/config/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 # Application definition
 
