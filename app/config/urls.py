12893bcb4d07842c37847feddde5ccada93d20eb
kiyoshion <kiyoshion@gmail.com>
2021-06-04 09:24:00 +0900

    Fixed config/urls.py

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-=======
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
->>>>>>> feature/item
     path('item/', include('item.urls')),
 ]
