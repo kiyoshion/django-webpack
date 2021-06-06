e654cdd75a569a681c622bc2ba6b91dc286e3137
kiyoshion <kiyoshion@gmail.com>
2021-06-06 10:55:25 +0900

    Update settings for production

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -4,9 +4,9 @@ if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-      sleep 0.1
-    done
+    # while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+    #   sleep 0.1
+    # done
 
     echo "PostgreSQL started"
 fi
