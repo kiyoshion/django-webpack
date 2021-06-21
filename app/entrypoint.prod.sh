055404a5e3e2e8aa5a31b09c1c28f4ce6be5bceb
kiyoshion <kiyoshion@gmail.com>
2021-06-21 16:53:42 +0900

    Update entrypoint for collectstatic

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -3,6 +3,7 @@
 python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
+npm install
 npm run build
 python manage.py collectstatic --no-input --clear
 
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate
+npm install
+npm run dev
 
 exec "$@"
